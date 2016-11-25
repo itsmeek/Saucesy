c4d55bdf7dde013b3ff60ab0f4614f0ec99be70f
itsmeek <meekyacquah@gmail.com>
2016-11-24 22:50:18 -0800

    ui created for collection view cell and new file recipe cell created

---
+++ Saucesy/RecipeCell.swift
@@ -0,0 +1,17 @@
+//
+//  RecipeCell.swift
+//  Saucesy
+//
+//  Created by Meeky Dekowski on 11/24/16.
+//  Copyright © 2016 Meek Apps. All rights reserved.
+//
+
+import UIKit
+
+class RecipeCell: UICollectionViewCell {
+    
+    @IBOutlet weak var recipeImage: UIImageView!
+    @IBOutlet weak var recipeName: UILabel!
+    @IBOutlet
+    
+}
