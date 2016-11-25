c5900d93e7adc74bebd086137996522fcb00b437
itsmeek <meekyacquah@gmail.com>
2016-11-24 17:35:17 -0800

    added bottom bar icons. Added code in the App deligate to change the color of the selected tab element

--- Saucesy/AppDelegate.swift
+++ Saucesy/AppDelegate.swift
@@ -16,6 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        UITabBar.appearance().tintColor = UIColor(red:0.92, green:0.33, blue:0.33, alpha:1.0)
         return true
     }
 
