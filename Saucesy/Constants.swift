38c4b0a5d0ff90875def337d62b08afb4caf85fb
itsmeek <meekyacquah@gmail.com>
2016-11-25 13:00:45 -0800

    Constants file added to place constants in

---
+++ Saucesy/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  Saucesy
+//
+//  Created by Meeky Dekowski on 11/25/16.
+//  Copyright © 2016 Meek Apps. All rights reserved.
+//
+
+import Foundation
+
+let URL_BASE = "https://api.edamam.com/search?_app_id=&_app_key=&q=basil"
+//let APP
